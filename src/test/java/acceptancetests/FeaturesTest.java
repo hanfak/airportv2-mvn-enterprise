0a47891e63f5069846a26caf25ba6915e066cbdc
Hanif Fakira <fakira.work@gmail.com>
2019-02-09 15:10:18 +0000

    Init commit - setup pom (wip)

---
+++ src/test/java/acceptancetests/FeaturesTest.java
@@ -0,0 +1,16 @@
+package acceptancetests;
+
+import com.googlecode.yatspec.junit.SpecRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpecRunner.class)
+public class FeaturesTest {
+
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
