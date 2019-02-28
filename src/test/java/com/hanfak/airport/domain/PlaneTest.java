bfb274dca6e7b57312e74a9bf9fa1ad34066525d
Hanif Fakira <fakira.work@gmail.com>
2019-02-28 10:52:40 +0000

    US2 - refactoring - Missing test for setting plane to Flying status

--- src/test/java/com/hanfak/airport/domain/PlaneTest.java
+++ src/test/java/com/hanfak/airport/domain/PlaneTest.java
@@ -10,11 +10,20 @@ import static com.hanfak.airport.domain.PlaneStatus.LANDED;
 public class PlaneTest implements WithAssertions {
 
   @Test
-  public void landsAPlane() {
+  public void planeHasLanded() {
     Plane plane = new Plane(planeId("A0001"), FLYING);
 
     Plane landedPlane = plane.land();
 
     assertThat(landedPlane.planeStatus).isEqualTo(LANDED);
+  }
+
+  @Test
+  public void planeIsFlying() {
+    Plane plane = new Plane(planeId("A0001"), LANDED);
+
+    Plane landedPlane = plane.fly();
+
+    assertThat(landedPlane.planeStatus).isEqualTo(FLYING);
   }
 }
